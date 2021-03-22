a340c45d4544a68dee395f8364c1ee02010cfa6c
Boemo Wame Mmopelwa <boemommopelwa@gmail.com>
2021-03-22 19:44:41 +0200

    Update Authenticate.java

--- Authenticate.java
+++ Authenticate.java
@@ -1,42 +1,63 @@
+package com.example.typingdna;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import com.typingdna.typingdnarecorderandroid.TypingDNARecorderMobile;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
-import com.typingdna.TypingDNARecorderMobile;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
 
-/*In order to capture accurate typing patterns you need to enable TypingDNA Overlay Service.
- This service draws an overlay over the activity. In order to draw the overlay you have to add
- "android.permission.SYSTEM_ALERT_WINDOW"  and "android.permission.TYPE_APPLICATION_OVERLAY" in your
- AndroidManifest file. It is also necessary to declare and enable the "com.typingdna.TypingDNAOverlayService"
- Please see the included "AndroidManifest" file. You can permanently disable the overlay service
- by setting  "private static Boolean overlayEnabled" to "false" in TypingDNARecorderMobile class;
- */
-
-public class Example extends AppCompatActivity {
-
+public class Authenticate extends AppCompatActivity {
+    String typingpattern;
+    private RequestQueue mRequestQueue;
     private TypingDNARecorderMobile tdna;
-    private EditText textField;
-    private TextView resultOutput;
+    private EditText password;
+    private EditText username;
+    private Button btnregister;
+    String tp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_example);
+        setContentView(R.layout.activity_main);
 
         //Initializes the typingDNA recorder and starts recording.
         tdna = new TypingDNARecorderMobile(this);
         tdna.start();
 
-        textField = findViewById(R.id.textField);
-        resultOutput = findViewById(R.id.resultOutput);
+        password = findViewById(R.id.password);
+        username = findViewById(R.id.username);
 
         //Adds a target to the recorder. You can add multiple elements. All the typing evens will be recorded for this component.
-        tdna.addTarget(R.id.textField);
-    }
+        tdna.addTarget(R.id.password);
 
+        btnregister = (Button) findViewById(R.id.btnregister);
+
+        btnregister.setOnClickListener(new View.OnClickListener() {
+                                          @Override
+                                          public void onClick(View v){
+
+                                              //sendAndRequestResponse();
+
+                                          }
+                                      }
+
+        );
+    }
     @Override
     protected void onDestroy() {
         //Stops the overlay service and ends the recording of further typing events.
@@ -68,9 +89,9 @@ public class Example extends AppCompatActivity {
         //Resets the history stack of recorded typing events.
         tdna.reset();
 
-        resultOutput.setText("");
-        textField.setText("");
-        textField.requestFocus();
+        //resultOutput.setText("");
+        //textField.setText("");
+        //textField.requestFocus();
     }
 
     public void getTypingPattern(View view){
@@ -87,17 +108,80 @@ public class Example extends AppCompatActivity {
                 break;
         }
         int length = 0; // (Optional) the length of the text in the history for which you want the typing pattern, 0 = ignore, (works only if text = "")
-        String text = textField.getText().toString(); // (Only for type 1 and type 2) a typed string that you want the typing pattern for
+        String text = password.getText().toString(); // (Only for type 1 and type 2) a typed string that you want the typing pattern for
         int textId = 0; // (Optional, only for type 1 and type 2) a personalized id for the typed text, 0 = ignore
         boolean caseSensitive = false; // (Optional, only for type 1 and type 2) Used only if you pass a text for type 1
-        Integer targetId = textField.getId(); //(Optional, only for type 1 and type 2) Specifies if pattern is obtain only from text typed in a certain target
-        String tp = tdna.getTypingPattern(type, length, text, textId, targetId, caseSensitive);
+        Integer targetId = password.getId(); //(Optional, only for type 1 and type 2) Specifies if pattern is obtain only from text typed in a certain target
+        tp = tdna.getTypingPattern(type, length, text, textId, targetId, caseSensitive);
         if(tp != null){
-            resultOutput.setText(tp);
+           // resultOutput.setText(tp);
         } else {
-            resultOutput.setText("");
+          //  resultOutput.setText("");
         }
     }
-    
-    // Making API requests
+
+        public  void savePattern (){
+
+            String  autoendpoint="";
+            String id= String.valueOf(username.getText()); //**
+            String postUrl = "https://api.typingdna.com/auto/" +id;
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+            JSONObject postData = new JSONObject();
+            try {
+                postData.put("typingpattern", tp );
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, postUrl, postData, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    System.out.println(response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    error.printStackTrace(); 
+                }
+            });
+
+            requestQueue.add(jsonObjectRequest);
+
+        }
+
+
+        public void verifyPattern(){
+            String url = "https://api.typingdna.com/user/:id?type={type}&textid={textid} ";
+            List<String> jsonResponses = new ArrayList<>();
+
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        JSONArray jsonArray = response.getJSONArray("data");
+                        for(int i = 0; i < jsonArray.length(); i++){
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            String email = jsonObject.getString("email");
+
+                            jsonResponses.add(email);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    error.printStackTrace();
+                }
+            });
+
+            requestQueue.add(jsonObjectRequest);
+
+
+        }
+
 }
